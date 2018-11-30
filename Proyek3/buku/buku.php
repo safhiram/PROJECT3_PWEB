ca129835d22a33d49bd2a4c3e22d141517105aaf
Unknown <safhira.maharani14@gmail.com>
2018-11-30 23:17:48 +0700

    Buku Baru satu halaman contoh

---
+++ Proyek3/buku/buku.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BUKU</title>
+	 <link rel="stylesheet" type="text/css" media="screen" href="stylebuku.css" />
+</head>
+<body>
+<div id="header">
+		<div id="atas">
+			<div id="atasleft">
+			<a href="#">
+			<img src="download.png" alt="Youtube" style="width: 200px;height: 60px;"></a>
+		</div>
+
+
+		<div id="atasright">
+        		
+        <div id="navbar">
+		<ul>
+			<li><a id="" href="#">Kategori Buku</a></li>
+  			<li><a id="" href="#">Bagaimana?</a></li>
+		</ul>
+	    </div>
+			<a href=""><button style="width:auto;">Login</button></a>
+            <a href=""><button style="width:auto;">Daftar</button></a>
+		</div>
+</div>
+<div id="kembali">
+	<div id="kembali1">
+		<img src="kembali.png" style="width: 5%;">
+	</div>
+	<div id="kembali2">
+		Microsof Excel
+		<img src="buku.png" style="width: 3%;">
+	</div>
+</div>
+<div id="deskripsi">
+	<div id="deskripsi1">
+		<h1>DESKRIPSI BUKU</h1>
+		<p>Buku ini berisi tentang belajar bagi pemula Microsof Excel</p>
+	</div>
+	<div id="gambar">
+		<img src="1.jpg" style="width: 200px;">
+		<br>
+		<button>RESERVASI</button>
+	</div>
+</div>
+
+</body>
+</html>
