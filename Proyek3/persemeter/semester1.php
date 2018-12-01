df95e73eb8832bbc87344e7acd59d9c84577b77a
Unknown <safhira.maharani14@gmail.com>
2018-12-01 22:49:38 +0700

    Daftar Buku Persemester

--- Proyek3/persemeter/semester1.php
+++ Proyek3/persemeter/semester1.php
@@ -3,6 +3,11 @@
 <head>
 	<title>SEMESTER 1</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js.js"></script>
 </head>
 <body>
 <div id="header">
@@ -11,7 +16,6 @@
 			<a href="#">
 			<img src="download.png" alt="Youtube" style="width: 200px;height: 60px;"></a>
 		</div>
-
 
 		<div id="atasright">
         		
@@ -24,6 +28,132 @@
 			<a href=""><button style="width:auto;">Login</button></a>
             <a href=""><button style="width:auto;">Daftar</button></a>
 		</div>
+      <div class="input-group" style="width: 20%; float: right;">
+        <input type="text" class="form-control"  id="myInput" placeholder="Search" name="search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+        </div>
+      </div>
 </div>
+<div class="container" style="width: 8%; float: right;">                   
+  <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Sort
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">ID Buku</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Judul Buku</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Terakhir ditambah</a></li>
+    </ul>
+  </div>
+</div>
+<div id="judul">
+	<h1>KATEGORI BUKU</h1>
+	<h4>-Semester 1-</h4>
+</div>
+
+<div class="container">
+  <h2>Daftar Buku</h2>
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">1</a></li>
+    <li><a data-toggle="tab" href="#menu4">2</a></li>
+    <li><a data-toggle="tab" href="#menu2">3</a></li>
+    <li><a data-toggle="tab" href="#menu3">4</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+		<div class="row">
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku1.jpg" style="width: 60%;">
+    		</div>
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+		</div>
+		<div class="row">
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku2.jpg" style="width: 60%;">
+    		</div>
+		</div>
+    </div>
+    <div id="menu4" class="tab-pane fade">
+		<div class="row">
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku3.jpg" style="width: 60%;">
+    		</div>
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+		</div>
+		<div class="row">
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku4.jpg" style="width: 60%;">
+    		</div>
+		</div>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+		<div class="row">
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku5.jpg" style="width: 60%;">
+    		</div>
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+		</div>
+		<div class="row">
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku6.jpg" style="width: 60%;">
+    		</div>
+		</div>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+		<div class="row">
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku7.jpg" style="width: 60%;">
+    		</div>
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+		</div>
+		<div class="row">
+    		<div class="col-sm-7" style="background-color:lavenderblush; height: 450px;padding-left: 5%;">
+    			<h2>Microsof Excel</h2>
+    			<p>Buku ini berisi tentang pembelajaran bagi pemula</p>
+    			<p>Untuk dapat menguasai Microsof Excel</p>
+    		</div>
+    		<div class="col-sm-5" style="background-color:lavender; height: 450px; padding-left: 12%; padding-top: 5%;">
+    			<img src="buku8.jpg" style="width: 60%;">
+    		</div>
+		</div>
+    </div>
+  </div>
+</div>
+
+
 </body>
 </html>
