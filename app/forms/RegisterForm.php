d4ff10165f46b2f7d442d75525c33359d66d7a54
ranggakd <ranggakd@users.noreply.github.com>
2018-12-17 13:46:25 +0700

    Update RegisterForm.php
    regex validator di form

--- app/forms/RegisterForm.php
+++ app/forms/RegisterForm.php
@@ -6,70 +6,83 @@ use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Element\Email;
 use Phalcon\Forms\Element\Password;
 use Phalcon\Forms\Element\Submit;
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Regex;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\StringLength;
 
 class RegisterForm extends Form
 {
     public function initialize()
     {
-        $this->add(
-            new Text(
-                'rname',
+        $name = new Text(
+            'rname',
+            [
+                'class' => "form-control",
+                'placeholder' => 'Raka',
+            ]
+        );
+        $nrp = new Text(
+            'rnrp',
+            [
+                'class' => "form-control",
+                'placeholder' => '05111740000120',
+            ]
+        );
+        $email = new Email(
+            'remail',
+            [
+                'class' => "form-control",
+                'placeholder'   =>  'ranggakusumad@gmail.com',
+            ]
+        );
+        $passold = new Password(
+            'rpasswordlama',
+            [
+                'class' => "form-control",
+            ]
+        );
+        $pass = new Password(
+            'rpassword',
+            [
+                'class' => "form-control",
+            ]
+        );
+        $passconf = new Password(
+            'rcpassword',
+            [
+                'class' => "form-control",
+            ]
+        );
+        $submit = new Submit(
+            'rsubmit',
+            [
+                'class' => "btn btn-primary",
+                'value'   =>  'Daftar',
+            ]
+        );
+        // here i left of
+        // $cvalid = new Validation();
+        $name->addValidator(
+            new PresenceOf(
                 [
-                    'class' => "form-control",
-                    'placeholder' => 'Raka',
+                    'message' => 'Nama username diisi!',
                 ]
             )
         );
-        $this->add(
-            new Text(
-                'rnrp',
+        $name->addValidator(
+            new StringLength(
                 [
-                    'class' => "form-control",
-                    'placeholder' => '05111740000120',
+                    'min' => 3,
+                    'messageMinimum' => 'Nama terlalu pendek!',
                 ]
             )
         );
-        $this->add(
-            new Email(
-                'remail',
-                [
-                    'class' => "form-control",
-                    'placeholder'   =>  'ranggakusumad@gmail.com',
-                ]
-            )
-        );
-        $this->add(
-            new Password(
-                'rpasswordlama',
-                [
-                    'class' => "form-control",
-                ]
-            )
-        );
-        $this->add(
-            new Password(
-                'rpassword',
-                [
-                    'class' => "form-control",
-                ]
-            )
-        );
-        $this->add(
-            new Password(
-                'rcpassword',
-                [
-                    'class' => "form-control",
-                ]
-            )
-        );
-        $this->add(
-            new Submit(
-                'rsubmit',
-                [
-                    'class' => "btn btn-primary",
-                    'value'   =>  'Daftar',
-                ]
-            )
-        );
+        $this->add($name);
+        $this->add($nrp);
+        $this->add($email);
+        $this->add($passold);
+        $this->add($pass);
+        $this->add($passconf);
     }
 }
