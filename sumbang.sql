25a1e34fa10eb9b2f4ceb202823189c31546ac44
ranggakd <ranggakd@users.noreply.github.com>
2018-12-18 13:56:31 +0700

    Dinamis Query Time

--- sumbang.sql
+++ sumbang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2018 at 12:48 PM
+-- Generation Time: Dec 18, 2018 at 07:55 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -41,8 +41,18 @@ CREATE TABLE `sumbang` (
   `deskripsi_bukusumbang` varchar(250) NOT NULL,
   `status` int(11) NOT NULL,
   `semester` int(11) NOT NULL,
-  `gambarsumbang` int(11) NOT NULL
+  `gambarsumbang` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sumbang`
+--
+
+INSERT INTO `sumbang` (`id_sumbang`, `nrp`, `nama`, `email`, `no_hp`, `departemen`, `universitas`, `judul_bukusumbang`, `jumlah_halamansumbang`, `deskripsi_bukusumbang`, `status`, `semester`, `gambarsumbang`) VALUES
+(1, '05111740001200', 'rangga', 'rangga@gmail.com', '08996569365', 'Informatika', 'ITS', 'C++ Pro', 123, 'hehe', 0, 0, 'buku-sumbang/C++ Pro'),
+(2, '05111740001200', 'rangga', 'rangga@gmail.com', '08996569365', 'Informatika', 'ITS', 'C++ Pro', 222, 'fefe', 0, 0, 'buku-sumbang/C++ Pro.jpg'),
+(3, '05111740001205', 'rangga', 'rangga@gmail.com', '08996569365', 'Informatika', 'ITS', 'C++ Pro', 787, 'reres', 0, 0, 'buku-sumbang/C++ Pro.jpg'),
+(4, '05111740001205', 'ade', 'rangga@gmail.com', '08996569365', 'Informatika', 'ITS', 'Advanced C++', 124, 'ini bagus', 0, 0, 'buku-sumbang/Advanced C++.jpg');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +72,7 @@ ALTER TABLE `sumbang`
 -- AUTO_INCREMENT for table `sumbang`
 --
 ALTER TABLE `sumbang`
-  MODIFY `id_sumbang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_sumbang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
